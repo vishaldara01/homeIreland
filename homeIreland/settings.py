2e7fabd85969fbf1231fb8470067a516ecef7d17
Vishal Dara <x00168022@myTUDublin.ie>
2020-07-09 01:50:22 +0100

    delete key

--- homeIreland/settings.py
+++ homeIreland/settings.py
@@ -145,12 +145,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-#Email config
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 465
-# EMAIL_HOST_USER= 'homeireland01@gmail.com'
-# EMAIL_HOST_PASSWORD='homeireland.1'
-# EMAIL_USE_TLS=True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PASSWORD = 'Saikirpa01'
