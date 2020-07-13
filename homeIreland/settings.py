046851c16671a5e7450fa07b3a517ab1ceb125c8
Vishal Dara <x00168022@myTUDublin.ie>
2020-07-13 13:10:59 +0100

    xxyyz55666

--- homeIreland/settings.py
+++ homeIreland/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'homeIreland/static'),
+)
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 # Media Folder Settings
