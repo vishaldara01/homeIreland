8dc7753606827aedad48a64f7cab03d8ba44e236
Vishal Dara <x00168022@myTUDublin.ie>
2020-07-22 13:49:28 +0100

    update wsgi

--- homeIreland/wsgi.py
+++ homeIreland/wsgi.py
@@ -1,5 +1,4 @@
-"""
-WSGI config for homeIreland project.
+"""WSGI config for homeIreland project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +7,18 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import environ
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
+
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homeIreland.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
